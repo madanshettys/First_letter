9c47b339c67469b52a49082c972f988e1d99eb52
madanshettys <madansshetty08@gmail.com>
2025-04-13 20:40:55 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
 
 class Message(BaseModel):
     text: str
+
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome to the First Letter API!"}
 
 @app.post("/process")
 async def process_input(msg: Message):
